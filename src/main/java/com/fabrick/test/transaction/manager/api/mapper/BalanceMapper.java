938e82b8dfcef27eed9e2aec23a18d88d3390d63
carolina <carolslc92@gmail.com>
2025-06-24 13:08:59 +0200

    fix typo

--- src/main/java/com/fabrick/test/transaction/manager/api/mapper/BalanceMapper.java
+++ src/main/java/com/fabrick/test/transaction/manager/api/mapper/BalanceMapper.java
@@ -7,5 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface BalanceMapper {
 
-    BalanceApiResponse toBalanceApiResponse(BalanceResponse gbsBalanceResponseResponse);
+    BalanceApiResponse toBalanceApiResponse(BalanceResponse gbsBalanceResponse);
 }
